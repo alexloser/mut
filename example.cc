3b16b11ea4266dcd804239e5d45787dd6c370fe1
alexloser <cypro666@gmail.com>
2019-11-09 13:21:59 +0800

    update

--- example.cc
+++ example.cc
@@ -42,8 +42,9 @@ MICRO_UNIT(Test_strequ)
 
 MICRO_UNIT(Test_const)
 {
-    MUT_DEBUG << "This is debug message" << std::endl;
-    ASSERT_FALSE((1024 << 2) == 2048)
+    const int SOMEVAR = 2;
+    MUT_DEBUG(SOMEVAR);
+    ASSERT_FALSE((1024 << SOMEVAR) == 2048)
     if (float(1.0000000001f) == float(1.0f)) {
         MUT_PASS()
     }
@@ -52,7 +53,7 @@ MICRO_UNIT(Test_const)
 
 MICRO_UNIT_TEST_MAIN
 {
-    bool ok = mut::unit_tester::run();
+    bool ok = mut::unit_tester::run(true);
     return ok ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
--- mut.h
+++ mut.h
@@ -2,52 +2,71 @@
  * Micro unit test headers-only library for cpp, need c++11 supported
  * Copyright, AlexLoser, Sebastiao Salvador, all rights reserved.
  * See https://github.com/alexloser/mut for more details.
+ * Example:
+ *
+ * MICRO_UNIT(TestAdd)
+ * {
+ *   int a = 2, b = 3;
+ *   ASSERT_EQ(a + b, 5);
+ * }
+ *
+ * MICRO_UNIT_TEST_MAIN
+ * {
+ *   bool pass = mut::unit_tester::run(true);
+ *   return pass ? EXIT_SUCCESS : EXIT_FAILURE;
+ * }
  */
 #pragma once
+#include <ctime>
+#include <cstdlib>
 #include <cstring>
 #include <string>
 #include <vector>
 #include <map>
 #include <iostream>
+#include <iomanip>
 
 // Linux terminator color code
-#ifndef MUT_COLOR_CODE
-#define MUT_COLOR_CODE
-#define MUT_COLOR_GRAY0  "\033[00;37m"
-#define MUT_COLOR_GRAY   "\033[01;37m"
-#define MUT_COLOR_GREEN  "\033[01;32m"
-#define MUT_COLOR_RED    "\033[01;31m"
-#define MUT_COLOR_YELLOW "\033[01;33m"
-#define MUT_COLOR_PASS   "\033[01;36m"
-#define MUT_COLOR_FAIL   "\033[01;35m"
+namespace mut {
+static const char* MUT_COLOR_GRAY0  = "";
+static const char* MUT_COLOR_GRAY   = "";
+static const char* MUT_COLOR_GREEN  = "";
+static const char* MUT_COLOR_RED    = "";
+static const char* MUT_COLOR_YELLOW = "";
+static const char* MUT_COLOR_PASS   = "";
+static const char* MUT_COLOR_FAIL   = "";
+}
+
+#ifndef MUT_STREAM
+#define MUT_STREAM std::cerr << mut::MUT_COLOR_GRAY << std::setw(6) << clock() << " "
 #endif
 
 // Macro for writing to the terminal an INFO-level log
 #ifndef MUT_INFO
-#define MUT_INFO        std::cerr << MUT_COLOR_YELLOW << "[INFO]  " << MUT_COLOR_GRAY << " "
+#define MUT_INFO        MUT_STREAM << mut::MUT_COLOR_YELLOW << "[INFO]  " << mut::MUT_COLOR_GRAY << " "
 #define MUT_LOG_INFO    MUT_INFO << __FILE__ << ":" << __LINE__ << ": "
 #endif
 
 // Macro for writing to the terminal a BAD-level log
 #ifndef MUT_BAD
-#define MUT_BAD         std::cerr << MUT_COLOR_RED << "[BAD!]  " << MUT_COLOR_GRAY << " "
+#define MUT_BAD         MUT_STREAM << mut::MUT_COLOR_RED << "[BAD!]  " << mut::MUT_COLOR_GRAY << " "
 #define MUT_LOG_BAD     MUT_BAD << __FILE__ << ":" << __LINE__ << ": "
 #endif
 
 // Macro for writing to the terminal a GOOD-level log
 #ifndef MUT_GOOD
-#define MUT_GOOD        std::cerr << MUT_COLOR_GREEN << "[GOOD]  " << MUT_COLOR_GRAY << " "
+#define MUT_GOOD        MUT_STREAM << mut::MUT_COLOR_GREEN << "[GOOD]  " << mut::MUT_COLOR_GRAY << " "
 #define MUT_LOG_GOOD    MUT_GOOD << __FILE__ << ":" << __LINE__ << ": "
 #endif
 
 // Macro for writing to the terminal an DEBUG info from tested program
 #ifndef MUT_DEBUG
-#define MUT_DEBUG       std::cerr << MUT_COLOR_GRAY0 << "[DEBUG]  "
+#define MUT_DEBUG(var)  MUT_STREAM << mut::MUT_COLOR_GRAY0 << "[DEBUG]  " #var " " << var << std::endl;
 #endif
 
 #ifndef MUT_LOG_SEP
 #define MUT_LOG_SEP \
-    std::cerr << "-------------------------------------------------------------------------------" << std::endl
+std::cerr << "-----------------------------------------------------------------------------------------" << std::endl
 #endif
 
 
@@ -65,24 +84,40 @@ typedef void (*unit_fun_t)(unit_function_result*);
 // Main class for unit test management. This class is a singleton.
 class unit_tester
 {
-protected:
-    unit_tester(){};
-
-    static unit_tester& instance()
-    {
-        static unit_tester instance;
-        return instance;
-    }
-
 public:
     ~unit_tester() {};
 
-    // Run all the registered unit test cases, returns True if all tests pass, false otherwise.
-    static bool run()
+    static void set_clr(bool color_mode)
     {
-        MUT_INFO << MUT_COLOR_YELLOW
-                 << "Will Run " << instance().unit_functions.size() << " Test Cases"
-                 << MUT_COLOR_GRAY << std::endl;
+        if (color_mode) {
+            MUT_COLOR_GRAY0 = "\033[00;37m";
+            MUT_COLOR_GRAY  = "\033[01;37m";
+            MUT_COLOR_GREEN = "\033[01;32m";
+            MUT_COLOR_RED   = "\033[01;31m";
+            MUT_COLOR_YELLOW= "\033[01;33m";
+            MUT_COLOR_PASS  = "\033[01;36m";
+            MUT_COLOR_FAIL  = "\033[01;35m";
+        }
+    }
+
+    static bool run(int argc, char* argv[])
+    {
+        bool color_mode = false;
+        for (int i = 1; i < argc; ++i) {
+            if (strstr(argv[i], "color_mode")) {
+                color_mode = true;
+            }
+        }
+        return unit_tester::run(color_mode);
+    }
+
+    // Run all the registered unit test cases, returns True if all tests pass, false otherwise.
+    static bool run(bool color_mode = false)
+    {
+        set_clr(color_mode);
+        std::cerr << MUT_COLOR_YELLOW
+                  << "\t\t\tWill Run " << instance().unit_functions.size() << " Test Cases"
+                  << MUT_COLOR_GRAY << std::endl;
 
         std::vector<std::string> failures, sucesses;
         // Starts unit tests
@@ -138,7 +173,7 @@ public:
     // Helper class to register a unit test in construction time. This is used to
     // call register_function in the construction of a static helper object.
     // Used by the MUT_REGISTER macro, which in turn is used by the MICRO_UNIT macro.
-    // returns True if all tests pass, false otherwise.
+    // Returns True if all tests pass, false otherwise.
     struct registrator
     {
         registrator(const std::string &name, unit_fun_t function)
@@ -146,6 +181,14 @@ public:
             unit_tester::register_function(name, function);
         };
     };
+
+protected:
+    unit_tester() { }
+    static unit_tester& instance()
+    {
+        static unit_tester instance;
+        return instance;
+    }
 
 protected:
     std::map<std::string, unit_fun_t> unit_functions;
@@ -157,81 +200,110 @@ protected:
 } // end namespace mut
 
 #ifndef MUT_USER_MACROS
+#define MUT_USER_MACROS
+
+#ifndef MUT_CAT_NEXP
 #define MUT_CAT_NEXP(A, B) A ## B
+#endif
+
+#ifndef MUT_MACRO_CAT
 #define MUT_MACRO_CAT(A, B) MUT_CAT_NEXP(A, B)
+#endif
 
 // Register a unit function using a helper static registrator object.
+#ifndef MUT_REGISTER
 #define MUT_REGISTER(function)                                           \
       static mut::unit_tester::registrator                               \
       MUT_MACRO_CAT(UNIT_REGISTRATION, __COUNTER__)(#function, function);
+#endif
 
+// Start a new unit test case block
+#ifndef MICRO_UNIT
 #define MICRO_UNIT(function)                                             \
     void function(mut::unit_function_result*);                           \
     MUT_REGISTER(function);                                              \
     void function(mut::unit_function_result* __mut_result)
+#endif
 
 // Pass the test and return from the test case.
+#ifndef MUT_PASS
 #define MUT_PASS() \
     {                                                               \
         MUT_LOG_GOOD << "Test stopped: Pass" << std::endl;          \
-        __mut_result->success = true;                     \
+        __mut_result->success = true;                               \
         return;                                                     \
     }
+#endif
 
 // Fail the test and return from the test case.
+#ifndef MUT_FAIL
 #define MUT_FAIL() \
     {                                                               \
         MUT_LOG_BAD << "Test stopped: Fail" << std::endl;           \
-        __mut_result->success = false;                    \
+        __mut_result->success = false;                              \
         return;                                                     \
     }
+#endif
 
 // Check a particular test condition. If the condition does not hold, fail the test and return.
+#ifndef ASSERT_TRUE
 #define ASSERT_TRUE(condition) \
     if (!(condition)) {                                                 \
         MUT_LOG_BAD << "Assert-True failed: " #condition << std::endl;  \
         MUT_FAIL();                                                     \
     }
+#endif
 
 // Check a particular test condition. If the condition holds, fail the test and return.
+#ifndef ASSERT_FALSE
 #define ASSERT_FALSE(condition) \
     if((condition)) {                                                   \
         MUT_LOG_BAD << "Assert-False failed: " #condition << std::endl; \
         MUT_FAIL();                                                     \
     }
+#endif
 
 // Check a particular test condition, equal
+#ifndef ASSERT_EQ
 #define ASSERT_EQ(X, Y) \
     if (!(X==Y)) {                                                          \
         MUT_LOG_BAD << "Assert-Equal failed: " #X " == " #Y << std::endl;   \
         MUT_FAIL();                                                         \
     }
+#endif
 
 // Check a particular test condition, not equal
+#ifndef ASSERT_NE
 #define ASSERT_NE(X, Y) \
     if (!(X!=Y)) {                                                           \
         MUT_LOG_BAD << "Assert-NotEqual failed: " #X " != " #Y << std::endl; \
         MUT_FAIL();                                                          \
     }
+#endif
 
 // Check a particular test condition, x less than y
+#ifndef ASSERT_LT
 #define ASSERT_LT(X, Y) \
     if (!(X<Y)) {                                                            \
         MUT_LOG_BAD << "Assert-LessThan failed: " #X " < " #Y << std::endl;  \
         MUT_FAIL();                                                          \
     }
+#endif
 
 // Check a particular test condition, x greater than y
+#ifndef ASSERT_GT
 #define ASSERT_GT(X, Y) \
     if (!(X>Y)) {                                                            \
         MUT_LOG_BAD << "Assert-GreatThan failed: " #X " > " #Y << std::endl; \
         MUT_FAIL();                                                          \
     }
+#endif
 
 
 #ifndef MICRO_UNIT_TEST_MAIN
-#define MICRO_UNIT_TEST_MAIN int main(int argc, char* argv[])
+#define MICRO_UNIT_TEST_MAIN int main(int argc, char* argv[], char* env[])
 #endif
+
 
 
 #endif  // end MUT_USER_MACROS
